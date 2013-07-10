f68d628db10ba93e0265ce39b99c63fdc7a45351
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2013-07-10 12:59:23 -0400

    fix indentation

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,10 +1,10 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-		
-		var stupid_fb_thing = document.getElementById('pagelet_ego_pane_w');
-		stupid_fb_thing.style.display = 'none';
-	}
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+			
+			var stupid_fb_thing = document.getElementById('pagelet_ego_pane_w');
+			stupid_fb_thing.style.display = 'none';
+		}
 	}, 10);
 });
